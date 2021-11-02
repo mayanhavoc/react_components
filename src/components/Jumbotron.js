3924f56c9dcb9d9823e7213174e2a77c9e8993e0
Roberto <mayanhavoc@gmail.com>
2021-11-01 17:27:18 -0700

    doesnt animate

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import DynamicBackground from './DynamicBackground';
+
 
 class Jumbotron extends Component {
     render() {
         return (
             <div>
-                <DynamicBackground />
+                
             </div>
         )
     }
